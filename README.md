1b2f5f43530aa53f69a041cbb5baeea172a4d3ae
Matt Schmoyer <mschmoyer@50onred.com>
2013-10-01 17:33:10 -0400

    Adds untappd client

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-untappd
+Untappd API Python Wrapper
 =======
 
-Python wrapper for the Untappd.com API
+Python wrapper for the Untappd.com API.
+
+# API
+* [Documentaion](https://untappd.com/api/docs)
+* [Register](https://untappd.com/api/register) for a client ID and client secret
---
+++ untappd.py
@@ -0,0 +1,71 @@
+import requests
+
+DEFAULT_BASE_URI = 'http://api.untappd.com/v4'
+BASE_URI = ''
+CLIENT_ID = ''
+CLIENT_SECRET = ''
+
+simple_endpoints = ['thepub', 'thepub/local', 'checkin/recent', 
+                    'beer/trending', 'checkin/add', 'user/pending', 
+                    'notifications', 'heartbeat']
+
+simple_endpoints_with_requires = [('user_wishlist_add', 'bid'), 
+                                  ('user_wishlist_delete', 'bid'),
+                                  ('search_beer', 'q'),
+                                  ('search_brewery', 'q')]
+
+single_param_endpoints = ['user/checkins', 'venue/checkins', 'beer/checkins', 
+                          'brewery/checkins', 'brewery/info', 'beer/info',
+                          'venue/info', 'checkin/view', 'user/info', 
+                          'user/badges', 'user/friends', 'user/wishlist',
+                          'user/beers', 'checkin/toast', 'friend/accept',
+                          'friend/reject', 'friend/remove', 'friend/request']
+
+class Untappd:
+
+    ACCESS_TOKEN = None
+
+    @staticmethod
+    def __make_simple_endpoint_fun(name):
+        @staticmethod
+        def _function(options={}):
+            return Untappd._get('/' + name, options)
+        return _function
+
+    @staticmethod
+    def __make_singlearg_endpoint_fun(name):
+        @staticmethod
+        def _function(id, options={}):
+            return Untappd._get('/' + name + '/' + id, options)
+        return _function
+
+    @staticmethod
+    def __make_simple_with_requires_fun(name_and_required):
+        name, required_param = name_and_required
+        @staticmethod
+        def _function(required_attribute, options={}):
+            options.update({required_param: required_attribute})
+            return Untappd._get('/' + name, options)
+        return _function
+
+    @staticmethod
+    def _get(request, options):
+        options.update({'client_id': Untappd.CLIENT_ID,
+                        'client_secret': Untappd.CLIENT_SECRET})
+        return requests.get(Untappd.BASE_URI + request, params=options).json()
+
+    @staticmethod
+    def configure(client_id, client_secret, baseuri=DEFAULT_BASE_URI):
+        Untappd.CLIENT_ID = client_id
+        Untappd.CLIENT_SECRET = client_secret
+        Untappd.BASE_URI = baseuri
+        for endpoint in simple_endpoints:
+            fun = Untappd.__make_simple_endpoint_fun(endpoint)
+            setattr(Untappd, endpoint.replace('/', '_'), fun)
+        for endpoint in single_param_endpoints:
+            fun = Untappd.__make_singlearg_endpoint_fun(endpoint)
+            setattr(Untappd, endpoint.replace('/', '_'), fun)
+
+    @classmethod
+    def authenticate():
+        pass
